cedbfa2957df0f7e764c658274de65e113c00cd2
Itamar <itamarpj00@hotmail.com>
2025-04-08 01:46:03 -0300

    feat: create a layout and added the components

--- desafio-dev-frontend/src/App.tsx
+++ desafio-dev-frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import {
-  createTheme,
-  ThemeProvider,
-  CssBaseline,
-  Typography,
-} from "@mui/material";
+import SearchBar from "./components/SearchBar";
+import WeatherDetails from "./components/WeatherDetails/WeatherDetails";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { WeatherResponse } from "./models/WeatherResponse.model";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import { useState } from "react";
 
 const theme = createTheme({
   typography: {
@@ -12,10 +12,39 @@ const theme = createTheme({
 });
 
 const App = () => {
+  const [cityData, setCityData] = useState<WeatherResponse | null>(null);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Typography>Hello World</Typography>
+      <div className="bg-neutral-100 min-h-dvh">
+        {/* Title Section */}
+        <Box padding={2} className="bg-sky-400">
+          <Container>
+            <Typography variant="h6" align="left" color="white">
+              W e a t h e r
+            </Typography>
+          </Container>
+        </Box>
+
+        {/* Main Section */}
+        <Container>
+          <Grid container spacing={1} style={{ paddingTop: "20px" }}>
+            <Grid size={12} className="flex w-full justify-center items-center">
+              <SearchBar setCityData={setCityData} />
+            </Grid>
+            <Grid size={12}>
+              {cityData ? (
+                <WeatherDetails city={cityData} />
+              ) : (
+                <Typography textAlign={"center"}>
+                  Nenhum dado encontrado
+                </Typography>
+              )}
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
     </ThemeProvider>
   );
 };
